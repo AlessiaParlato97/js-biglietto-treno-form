24458f1f46a18f3cad0a037e80d28b6093781b51
Alessia Parlato <alessiaparlato@hotmail.it>
2024-07-04 17:28:39 +0200

    C'è qualquadra che non cosa

--- js.js
+++ js.js
@@ -34,6 +34,26 @@ formElement.addEventListener('submit', function (event) {
     console.log(selectSconto);
 
     const euro = 0.21;
+
+    if (selectSconto === '1') {
+        let calcoloSconto18 = (prezzoPerKm * 20) / 100;
+        console.log('Lo sconto applicato per i minori è di ' + calcoloSconto18 + ' euro');
+
+        let prezzoScontato18 = (prezzoPerKm - calcoloSconto18);
+        risultato = prezzoScontato18.toFixed(2);
+        console.log('Il prezzo scontato del biglietto è di ' + risultato + ' euro');
+    } else if (selectSconto === '2') {
+        let calcoloSconto65 = (prezzoPerKm * 40) / 100;
+        console.log('Lo sconto applicato per gli over 65 è di ' + calcoloSconto65 + ' euro');
+
+        let prezzoScontato65 = (prezzoPerKm - calcoloSconto65);
+        risultato = prezzoScontato65.toFixed(2);
+        console.log('Il prezzo scontato del biglietto è di ' + risultato + ' euro');
+
+    }
+
+
+
     let calcoloPrezzoTotale = km * euro;
 
     console.log(calcoloPrezzoTotale);
